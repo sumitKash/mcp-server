c6ab89abbb6c937063279eeadae23d18cd8e0723
Kashyap <sumit.kashyap@adidas.com>
2025-12-31 15:53:36 +0530

    added MCP client file

---
+++ client.py
@@ -0,0 +1,39 @@
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+import asyncio   # This will allow to run async call/task
+import traceback # For error handling
+
+
+# Creates new object called server_params 
+# If your server file is in diff folder then client.py , speficy that in args 
+server_params = StdioServerParameters(
+    command="npx", # This is equivalent to MCP Host client config  
+    args=["-y", "@openbnb/mcp-server-airbnb", "--ignore-robots-txt"],  # Optional command line arguments
+)
+
+
+async def run():
+    try:
+        print("Starting stdio_client...")
+        async with stdio_client(server_params) as (read, write):
+            print("Client connected, creating session...")
+            async with ClientSession(read, write) as session:
+
+                print("Initializing session...")
+                await session.initialize()
+
+                print("Listing tools...")
+                tools = await session.list_tools()
+                print("Available tools:", tools)
+
+                print("Calling tool...")
+                result = await session.call_tool("airbnb_search", arguments={"location": "DelhiNCR"})
+
+                print("Tool result:", result)
+
+    except Exception as e:
+        print("An error occurred:")
+        traceback.print_exc()
+
+if __name__ == "__main__":
+    asyncio.run(run())
--- weather.py
+++ weather.py
@@ -2,7 +2,10 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("weather")
 
-@mcp.tool()
+# Docstring in below fucntion is important bz MCP client is actually able to grab the tool
+# and then pass it to the LLM 
+ 
+@mcp.tool() #decorator, elevated the below function to status of a tool
 def get_weather(location: str) -> str:
       """"
           gets the weather given a location
